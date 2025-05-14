c64cae023edc99198f3214121eeb15fb68475716
Scetch <tokisakiscetch@gmail.com>
2025-05-14 16:33:09 +0300

    Update game.cpp
    add counter

--- game.cpp
+++ game.cpp
@@ -37,10 +37,39 @@ std::string getHiddenWord(const std::string& word) {
     return hidden;
 }
 
+void revealLetters(std::string& hiddenWord, const std::string& secretWord) {
+    int lettersToReveal = 0;
+    int wordLength = secretWord.length();
+
+    if (wordLength == 3) {
+        lettersToReveal = 1;
+    }
+    else if (wordLength <= 5) {
+        lettersToReveal = 2;
+    }
+    else if (wordLength <= 9) {
+        lettersToReveal = 3;
+    }
+
+    if (lettersToReveal == 0) return;
+
+    for (int i = 0; i < lettersToReveal; ) {
+        int pos = std::rand() % wordLength;
+        if (hiddenWord[pos * 2] == '_') {
+            hiddenWord[pos * 2] = secretWord[pos];
+            i++;
+        }
+    }
+}
+
 int main() {
+    int count{ 0 };
     std::srand(std::time(0));
     std::string secretWord = getRandomWord();
     std::string hiddenWord = getHiddenWord(secretWord);
+
+    revealLetters(hiddenWord, secretWord);
+
     std::string guessedLetters;
     std::cout << "Welcome to Hangman!\n";
     std::cout << "Guess the word: " << hiddenWord << "\n";
@@ -49,6 +78,7 @@ int main() {
         char guess;
         std::cout << "Enter a letter: ";
         std::cin >> guess;
+        count++;
 
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
@@ -67,6 +97,10 @@ int main() {
                     hiddenWord[i * 2] = secretWord[i]; // * 2 (cuz of 'space')
                 }
             }
+            
+            if (count > 0) {
+                count--;
+            }
         }
         else {
             std::cout << "Wrong! Try again.\n";
@@ -75,7 +109,7 @@ int main() {
         std::cout << "Word: " << hiddenWord << "\n";
 
         if (hiddenWord.find('_') == std::string::npos) {
-            std::cout << "Congratulations! You won! The word was: " << secretWord << "\n";
+            std::cout << "Congratulations! You won! The word was: " << secretWord << "\n" << "Count -> " << count;
             break;
         }
     }
